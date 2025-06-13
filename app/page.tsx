d17f8db21a8ec116959cbe4f06d65a9cbee0b59b
Farhanalh1 <alhabsyifarhan31@gmail.com>
2025-06-13 21:39:20 +0700

    Initial commit for Project Akhir PBS by Farhanalh1

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button
+      size="lg"
+      variant="destructive"
+      >
+        Click me
+        </Button>
+  
+    </div>
+  );
+}
