423b04ca6c571a840fdd04a8084ea80f995ff583
Farhanalh1 <alhabsyifarhan31@gmail.com>
2025-06-14 17:54:02 +0700

    mengedit file store-modal.tsx lagi

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -5,17 +5,13 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import Modal from "../ui/modal";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";  
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 const formSchema = z.object({
     name: z.string().min(1),
 });
-
-const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-        name: "",
-    },
-})
 
 const onSubmit= async (values: z.infer<typeof formSchema>) => {
     //TODO Buat Toko
@@ -24,6 +20,14 @@ const onSubmit= async (values: z.infer<typeof formSchema>) => {
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        name: "",
+    },
+})
+
     return (
         <Modal
             title="Buat Store"
@@ -31,7 +35,33 @@ export const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}
         >
-            Store Form
+        <div>
+            <div>
+                <Form {...form}>
+                <form>
+                    <FormField
+                    control={form.control}
+                    name='name'
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input 
+                                placeholder='Nama Toko'
+                                {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <div>
+                        <Button>Cancel</Button>
+                    </div>
+                </form>
+                </Form>
+            </div>
+        </div>
         </Modal>
     )
 }
