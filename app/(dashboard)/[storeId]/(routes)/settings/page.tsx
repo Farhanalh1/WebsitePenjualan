a53fc3d28f345e72364b4f2ff96a453466f99c0a
Farhanalh1 <alhabsyifarhan31@gmail.com>
2025-06-25 16:40:56 +0700

    mengimport React

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,9 +1,27 @@
-const SettingsPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface SettingsPageProps {
+    params: {
+        storeId: string;
+    }
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = ({
+    params
+}) => {
+
+    const {userId} = auth()
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+
     return(
         <div>
             Ini Page Setting
         </div>
     );
 }
-
 export default SettingsPage;
