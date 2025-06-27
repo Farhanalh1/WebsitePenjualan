d257e8557b8a41a49f49d1529793450145bd9a35
Farhanalh1 <alhabsyifarhan31@gmail.com>
2025-06-27 18:40:05 +0700

    add const onSubmit

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -34,6 +34,10 @@ export const Settingsform: React.FC<SettingsFormProps> = ({
         defaultValues: initialData,
     });
 
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log()
+    }
+
     return (
         <>
         <div className=" flex items-center justify-between">
